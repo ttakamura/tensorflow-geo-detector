35fb84354e9913c7997d400482addf63df58f148
Tatsuya Takamura <takamura@tonchidot.com>
2016-06-07 15:59:26 +0900

    [from now] Tue Jun  7 15:59:26 JST 2016

--- tool/create_numpy_data.py
+++ tool/create_numpy_data.py
@@ -25,44 +25,43 @@ def add_vocab(text):
     vocab_counter[text] += 1
   return vocabrary[text]
 
-def open_csv(csv_file)
+def open_csv(doc_dir, csv_file):
+  global data_num
+  global max_step
+  with open(csv_file, 'r') as f:
+    csv_reader = csv.reader(f)
+    for row in csv_reader:
+      data_num += 1
+      step = 0
+      id, url, hash, name, addr = row
+      html_file = ('%s/%s' % (doc_dir, hash))
+      with open(html_file, 'r') as hf:
+        tsv_reader = csv.reader(hf, delimiter='\t')
+        for tsv_row in tsv_reader:
+          step += 1
+          if len(tsv_row) == 2:
+            token, category = tsv_row
+            token = token.decode('utf-8')
+            yield data_num, step, token
+      if max_step < step:
+        max_step = step
 
-with open(csv_file, 'r') as f:
-  csv_reader = csv.reader(f)
+for id, step, token in open_csv(doc_dir, csv_file):
+  add_vocab(token)
 
-  for row in csv_reader:
-    data_num += 1
-    step = 0
-    id, url, hash, name, addr = row
-    html_file = ('%s/%s' % (doc_dir, hash))
+max_vocab   = len(vocabrary)
+x_data_num  = data_num
+x_max_step  = max_step
+x_max_vocab = max_vocab
 
-    with open(html_file, 'r') as hf:
-      step += 1
-      tsv_reader = csv.reader(hf, delimiter='\t')
+shape = (x_max_step, x_max_vocab)
+mat   = lil_matrix((shape))
 
-      for tsv_row in tsv_reader:
-        if len(tsv_row) == 2:
-          token, category = tsv_row
-          token = token.decode('utf-8')
-          add_vocab(token)
+data_num = 0
+max_step = 0
 
-    if max_step < step:
-      max_step = step
-
-    print(data_num)
-
-max_vocab = len(vocabrary)
-
-shape = (max_step, max_vocab)
-mat = lil_matrix(shape)
-
-with open(csv_file, 'r') as f:
-  csv_reader = csv.reader(f)
-  for row in csv_reader:
-    id, url, hash, name, addr = row
-    html_file = ('%s/%s' % (doc_dir, hash))
-    with open(html_file, 'r') as hf:
-      tsv_reader = csv.reader(hf, delimiter='\t')
-      for tsv_row in tsv_reader:
-        token, category = tsv_row
-        token = token.decode('utf-8')
+for id, step, token in open_csv(doc_dir, csv_file):
+  mat[step, vocabrary[token]] = 1.0
+  if id == 2:
+    # print(mat)
+    1/0
