85af176e15dd2612c39d160ea55324430839462c
Tatsuya Takamura <takamura@tonchidot.com>
2016-06-08 18:08:47 +0900

    [from now] Wed Jun  8 18:08:47 JST 2016

--- model.py
+++ model.py
@@ -62,18 +62,18 @@ def LSTM(x, y):
   W_out = weight_variable([FLAGS.hidden_size, FLAGS.out_size])
   b_out = bias_variable([FLAGS.out_size])
   predictions = list()
+  cost_all    = list()
 
   with tf.variable_scope('lstm1') as scope:
     lstm_cell = rnn_cell.BasicLSTMCell(FLAGS.hidden_size, forget_bias=1.0)
     outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
-
     for output in outputs:
       pred = tf.matmul(output, W_out) + b_out
       predictions.append(pred)
+      cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+      cost_all.append(cost)
 
-    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
-
-  return pred, cost
+  return predictions, cost_all
 
 # ------- reshaping -----------------------------------
 def assert_reshaped_x(x, vocab_size):
