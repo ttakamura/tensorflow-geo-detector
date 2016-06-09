6e062ba2f9a84c3a99511e04ecc726b8758d5e26
Tatsuya Takamura <takamura@tonchidot.com>
2016-06-09 17:23:53 +0900

    [from now] Thu Jun  9 17:23:53 JST 2016

--- restore_chainer.py
+++ restore_chainer.py
@@ -2,6 +2,13 @@ import chainer
 from chainer import serializers
 
 import cmain
+import sys
 
 model = cmain.model()
-print(model)
+
+print(sys.argv[0])
+print(sys.argv[1])
+
+1/0
+
+serializers.load_npz('', model)
