8b004f5efd0ed4484e8ebd70df42b54068b35d72
Tatsuya Takamura <takamura@tonchidot.com>
2016-06-09 17:28:36 +0900

    [from now] Thu Jun  9 17:28:36 JST 2016

--- restore_chainer.py
+++ restore_chainer.py
@@ -9,4 +9,8 @@ model    = cmain.model()
 npy_file = sys.argv[1]
 serializers.load_npz(npy_file, model)
 
+xdata, ydata, zdata, ids, vocabrary = reader.load_master_data('tabelog_final_s')
+allx, ally, allz = reader.load_train_data(ids, xdata, ydata, zdata, batch_size, steps, vocab_size, out_size)
+train_x_data, test_x_data, train_y_data, test_y_data, train_z_data, test_z_data = reader.split_data(allx, ally, allz)
+
 embed()
