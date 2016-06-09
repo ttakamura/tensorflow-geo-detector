7a2b4df3372d3c411dddca809caf6f6eab86f07f
Tatsuya Takamura <takamura@tonchidot.com>
2016-06-09 15:48:04 +0900

    [from now] Thu Jun  9 15:48:04 JST 2016

--- cmain.py
+++ cmain.py
@@ -67,11 +67,10 @@ for epoch in range(20):
   print('epoch %d' % epoch)
   for i in range(len(train_x_data)):
     x = Variable(train_z_data[i])
-
     t = Variable(train_y_data[i].argmax(2).astype(np.int32))
     # t = Variable(train_y_data[i])
-
     optimizer.zero_grads()
     loss, outputs = rnn(x, t)
     loss.backward()
+    print("loss %f" % loss.data)
     optimizer.update()
